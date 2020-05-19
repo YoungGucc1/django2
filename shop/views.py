bd811eee1358d5574770f12ee8da0406a7162108
Dizzle <dilfar@mail.ru>
2020-05-19 15:46:14 +0600

    1 commit

--- shop/views.py
+++ shop/views.py
@@ -5,11 +5,13 @@ from .models import Article
 
 
 def home(request):
-    article = Article.objects.all()
-    res = '<h1>Список Новостей</h1>'
-    for item in article:
-        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
-    return HttpResponse(res)
+    article = Article.objects.order_by('-created_at')
+    context = {
+        'article': article,
+        'title': 'Список статей'
+    }
+    return render(request, 'shop/index.html', context)
+
 
 def shop(request):
     return HttpResponse('<h1>This is the shop</h1>')
