e1c94edf72bbab1b76ab0bae7538c96ce6862ee5
Dizzle <dilfar@mail.ru>
2020-05-30 16:09:17 +0600

    6 commit

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .views import *
 
 
@@ -6,5 +7,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('shop/', shop),
     path('category/<int:category_id>/', get_category, name='category'),
-    path('article/<int:article_id>', view_article, name='view_article')
+    path('article/<int:article_id>/', view_article, name='view_article'),
+    path('shop/add-article', add_article, name='add_article'),
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Article, Category
+from .forms import ArticleForm
 
 
 def home(request):
@@ -25,6 +26,14 @@ def view_article(request, article_id):
     article_item = get_object_or_404(Article, pk=article_id)
 
     return render(request, 'shop/view_article.html', {"article_item": article_item})
+
+
+def add_article(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = ArticleForm()
+    return render(request, 'shop/add_article.html', {'form': form})
 
 
 def shop(request):
